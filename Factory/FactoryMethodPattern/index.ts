021088b71629879c50550229df55c26947a74585
Matej Beno <matobeno1@gmail.com>
2021-09-01 11:23:46 +0200

    Fixes in Factory, starting Strategy

--- Factory/FactoryMethodPattern/index.ts
+++ Factory/FactoryMethodPattern/index.ts
@@ -1,10 +1,8 @@
 import { EnemyFactory } from "./EnemyFactory"
 
 const factory = new EnemyFactory();
-console.log("Main")
 
 for (let i = 0; i < 5; i++) {
     const enemy = factory.createEnemy();
     console.log(enemy.getName(), "with health:", enemy.getHealth());
-    5
 }
